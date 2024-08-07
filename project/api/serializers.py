1f21662c78909068b9a7a407765c66cd15a5df34
jyoti ade <jyotiade287@gmail.com>
2024-08-07 18:50:47 +0530

    remark

---
+++ project/api/serializers.py
@@ -0,0 +1,9 @@
+# import serializer from rest_framework
+from rest_framework import serializers
+from .models import MovieModel
+
+class MovieSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = MovieModel
+		
+		fields = ('id','title', 'description')
