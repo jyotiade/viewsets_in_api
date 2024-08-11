39209b21dfe21a8fdf0217ab996a7bb4a20360b6
jyoti ade <jyotiade287@gmail.com>
2024-08-11 22:48:09 +0530

    remark

--- project/api/models.py
+++ project/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
   
 class MovieModel(models.Model):
     title = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 500)
     
     class Meta:
         verbose_name = "MovieModel"
