33877208bcbccd2164455a4cb25fe9fa5b6250f0
jyoti ade <jyotiade287@gmail.com>
2024-08-13 23:04:37 +0530

    remark

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
 from rest_framework import routers 
 from .views import MovieViewSet
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
   
 # define the router
 router = routers.DefaultRouter()
@@ -8,5 +13,7 @@ router.register(r'movie', MovieViewSet, basename='movie')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
